23ad86e016cffc57730882ae3ace5f75ba7249fb
matinjiri <matinjiri.git@gmail.com>
2024-09-23 15:30:37 +0100

    feat: database config and typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import databaseConfig from "./config/database.config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: [
+        ".env",
+      ]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => config.get("database") 
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
