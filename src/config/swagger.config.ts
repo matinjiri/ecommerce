de8ea7899fdda6b3d763917b01f03255ac4dc80c
matinjiri <matinjiri.git@gmail.com>
2024-09-23 14:28:47 +0100

    feat: swagger configuration added successfully.

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle("E-Commerce API")
+    .setDescription("API documentation for the eCommerce platform")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+}
