6c26e20d16e1a26af4e49204e65bc298d9e8923a
matinjiri <matinjiri.git@gmail.com>
2024-09-25 09:59:24 +0100

    feat: redis added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,27 @@ import { AppController } from "./app.controller";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import databaseConfig from "./config/database.config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { CacheModule } from "@nestjs/cache-manager";
+import redisConfig from "./config/redis.config";
+import { RedisService } from "./shared/services/cach/redis.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig],
-      envFilePath: [
-        ".env",
-      ]
+      load: [databaseConfig, redisConfig],
+      envFilePath: [".env"],
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) => config.get("database") 
-    })
+      useFactory: async (config: ConfigService) => config.get("database"),
+    }),
+    CacheModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => config.get("redis"),
+    }),
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [RedisService],
 })
 export class AppModule {}
