46a67deae2f6e3ee97f784d6e18acc3e47306d43
matinjiri <matinjiri.git@gmail.com>
2024-09-27 13:14:06 +0100

    feat: user module added

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "src/database/entities/user.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) {}
+
+  async getUserByEmail(email: string): Promise<User> {
+    return await this.userRepository.findOne({ where: { email } });
+  }
+  async getUserByPhoneNumber(phoneNumber: string): Promise<User> {
+    return await this.userRepository.findOne({ where: { phoneNumber } });
+  }
+  async getUserById(id: number): Promise<User> {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+  async saveUser(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+}
