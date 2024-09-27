8778ff3ff80a90eb92aaf72b9a60bbb89de6e93b
matinjiri <matinjiri.git@gmail.com>
2024-09-27 13:42:19 +0100

    feat: login implemented (pend mail service)

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,17 +3,19 @@ import { AuthService } from "./auth.service";
 import { LoginDto } from "src/common/dtos/auth/login.dto";
 import VerifyOtpDto from "src/common/dtos/otp/verify-otp";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
   constructor(
     private authService: AuthService,
-  ){}
-  @Post('login')
+  ) {}
+  @Post("login")
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
-  @Post('otp/verify')
+  @Post("otp/verify")
   verifyOtp(@Body() verifyOtpDto: VerifyOtpDto) {
     return this.authService.verifyOtp(verifyOtpDto);
   }
+  @Post("signup")
+  signup() {}
 }
--- src/modules/auth/strategies/email-login.strategy.ts
+++ src/modules/auth/strategies/email-login.strategy.ts
@@ -1,9 +1,20 @@
 import { LoginDto } from "src/common/dtos/auth/login.dto";
 import { ILoginStrategy } from "../interfaces/login-strategy.interface";
 import { User } from "src/database/entities/user.entity";
+import { UserService } from "src/modules/user/user.service";
+import { Injectable, NotFoundException } from "@nestjs/common";
 
+@Injectable()
 export class EmailLoginStrategy implements ILoginStrategy {
+  constructor(
+    private userService: UserService
+  ){}
   async login(loginDto: LoginDto) : Promise<User>{
-    return (await { id: 1, email: "matin@gmail.com", phoneNumber: "09103980355" }) as User;
+    const foundUser = await this.userService.getUserByEmail(loginDto.email);
+    if (!foundUser)
+      throw new NotFoundException(
+        "user with this email has not registered"
+      );
+    return foundUser;
   }
 }
